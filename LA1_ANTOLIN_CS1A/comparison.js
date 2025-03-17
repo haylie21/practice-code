03052a4183c89fd690c783d93d4827ac94d05494
haylie21 <haylie.antolin08@gmail.com>
2025-03-18 03:18:55 +0800

    Create comparison.js

---
+++ LA1_ANTOLIN_CS1A/comparison.js
@@ -0,0 +1,23 @@
+// My favorite numbers
+let firstFavNumb = 88;
+let secondFavNumb = 21;
+
+// Performing the following comparisons
+let greater_than = firstFavNumb > secondFavNumb;
+let less_than = firstFavNumb < secondFavNumb;
+let greaterThan_equal = firstFavNumb >= secondFavNumb;
+let lessThan_equal = firstFavNumb <= secondFavNumb;
+let strict_equality = firstFavNumb === secondFavNumb;
+let loose_equality = firstFavNumb == secondFavNumb;
+let strict_inequality = firstFavNumb !== secondFavNumb;
+let loose_inequality = firstFavNumb != secondFavNumb;
+
+// Printing the results
+console.log("Greater Than:", greater_than);
+console.log("Less Than:", less_than);
+console.log("Greater Than or Equal to:", greaterThan_equal);
+console.log("Less Than or Equal to:", lessThan_equal);
+console.log("Strict Equality:", strict_equality);
+console.log("Loose Equality:", loose_equality);
+console.log("Strict Inequality:", strict_inequality);
+console.log("Loose Inequality:", loose_inequality);

7101d8cd0ab4b4bc496c502315357723b712341d
haylie21 <haylie.antolin08@gmail.com>
2025-03-18 03:17:40 +0800

    Create numbers.js

---
+++ LA1_ANTOLIN_CS1A/numbers.js
@@ -0,0 +1,21 @@
+// Variables
+let firstFavNum = 90;
+let secondFavNum = 110;
+
+// Performing operations
+let addition = firstFavNum + secondFavNum;
+let subtraction = firstFavNum - secondFavNum;
+let multiplication = firstFavNum * secondFavNum;
+let division = firstFavNum / secondFavNum;
+let modulus = firstFavNum % secondFavNum;
+let exponentiation = firstFavNum ** secondFavNum;
+
+// Printing the output
+console.log(`
+Addition: ${addition}
+Subtraction: ${subtraction}
+Multiplication: ${multiplication}
+Division: ${division}
+Modulus: ${modulus}
+Exponentiation (Power): ${exponentiation}
+`);

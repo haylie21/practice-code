06e4aef41aa5b899588b90638487f1ac862c1370
haylie21 <haylie.antolin08@gmail.com>
2025-03-18 03:15:40 +0800

    Create variables.js

---
+++ LA1_ANTOLIN_CS1A/variables.js
@@ -0,0 +1,7 @@
+// input variable
+let name = " Haylie Antolin";
+let whatDoYouWannaBecomeInYourLife = " Software Engineer";
+let gender = " Female";
+let twitterHandle = " N/A";
+// printing the output
+console.log(name + "\n" + whatDoYouWannaBecomeInYourLife + "\n" + gender + "\n" + twitterHandle )
